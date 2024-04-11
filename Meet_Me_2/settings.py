91ce9505ae704d40b63dbf2b510b5f312f6e57e1
Saftarli <tofiqsefterli@gmail.com>
2024-04-11 14:17:22 +0400

    feat: create contact page

--- Meet_Me_2/settings.py
+++ Meet_Me_2/settings.py
@@ -53,13 +53,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Meet_Me_2.urls'
+ROOT_URLCONF = "Meet_Me_2.urls"
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from .views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
-    path('', contact, name='contact'),
+    path('contact', contact,  name='contact'),
 
 ]
