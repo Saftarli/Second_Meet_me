41028602c31ad7038071a4a00c36b491bdd22ee5
Saftarli <tofiqsefterli@gmail.com>
2024-04-21 23:39:02 +0400

    feat: create general settings model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class GeneralSettings(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+    )
+    description = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+    )
+    parameter = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+    )
+    updated_data = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+    )
+    created_data = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+    )
